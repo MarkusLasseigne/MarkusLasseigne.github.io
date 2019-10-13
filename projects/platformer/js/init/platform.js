ddc01904e1ce80074cf8bdefa21fcaa7dda1f592
MarkusLasseigne <54724709+MarkusLasseigne@users.noreply.github.com>
2019-10-12 20:55:15 -0500

    Update platform.js

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -8,28 +8,32 @@
     /**
      * Platform code example:
      *      platform.create(x, y, scaleX, scaleY);
+     *      platform.create(horizontalpos, verticalpos, width, height)
      */ 
     platform.init = function (game) {
         //Floor or ground//
-        platform.create(0, game.world.height - 32, 3, 2);
-        platform.create(0, 585, 0.7, 5);
+        platform.create(0, game.world.height - 10, 3, 0.5);
+        platform.create(0, 575, 0.589, 5);
         //Top platform//
-        platform.create(0, 125, 1.95, 0.6);
+        platform.create(0, 115, 2.25, 1);
         //Step Walls and Jump Barriers//
-        platform.create(380, 500, 0.10, 10);
-        platform.create(410, 500, 0.02, 10);
-
-        platform.create(500, 415, 0.10, 15);
-        platform.create(530, 415, 0.02, 15); 
-
-        platform.create(620, 330, 0.10, 15);
-        platform.create(650, 330, 0.02, 15);
-
-        platform.create(740, 245, 0.10, 20);
-        platform.create(770, 245, 0.02, 20);
-
-        platform.create(860, 160, 0.10, 20);
-        platform.create(890, 160, 0.02, 20);
-
+        //1st Wall//
+        platform.create(380, 480, 0.10, 10);
+        platform.create(410, 480, 0.02, 10);
+        //2nd Wall
+        platform.create(500, 416, 0.10, 10);
+        platform.create(530, 416, 0.02, 10);
+        //3rd Wall
+        platform.create(620, 352, 0.10, 15);
+        platform.create(650, 352, 0.02, 15); 
+        //4th Wall
+        platform.create(740, 288, 0.10, 15);
+        platform.create(770, 288, 0.02, 15);
+        //5th Wall
+        platform.create(860, 224, 0.10, 20);
+        platform.create(890, 224, 0.02, 20);
+        //6th Wall
+        platform.create(980, 160, 0.10, 20);
+        platform.create(980, 160, 0.02, 20);
     };
 })(window);
