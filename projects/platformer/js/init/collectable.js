eb50b9b3d5af01a50fa5f50bd1c76b642063178f
MarkusLasseigne <54724709+MarkusLasseigne@users.noreply.github.com>
2019-10-12 20:57:04 -0500

    Update collectable.js

--- projects/platformer/js/init/collectable.js
+++ projects/platformer/js/init/collectable.js
@@ -3,7 +3,6 @@
     window.opspark = window.opspark || {};
     window.opspark.collectable = window.opspark.collectable || {};
     let collectable = window.opspark.collectable;
-    
     let type = {
         db: {assetKey: 'db', points: 10},
         max: {assetKey: 'max', points: 20},
@@ -11,20 +10,20 @@
         grace: {assetKey: 'grace', points: 40},
         kennedi: {assetKey: 'kennedi', points: 50}
     };
-
     /**
      * Collectable example:
      *      collectable.create(type, x, y, gravity, bounce);
      */ 
     collectable.init = function (game) {
-        collectable.create(type.db, 380, 450, 0);
+        collectable.create(type.db, 333, 666, 0);
 
-        collectable.create(type.grace, 790, 600, 0);
-        collectable.create(type.steve, 660, 600, 0);
-        collectable.create(type.max, 550, 600, 0); 
+        collectable.create(type.grace, 790, 650, 0);
 
-        collectable.create(type.kennedi, 200, 70, 0);
-        collectable.create(type.kennedi, 600, 70, 0);
+        collectable.create(type.steve, 666, 675, 0);
 
+        collectable.create(type.max, 1025, 645, 0);
+
+        collectable.create(type.kennedi, 200, 60, 0);
+        collectable.create(type.kennedi, 600, 60, 0);
     };
 })(window);
