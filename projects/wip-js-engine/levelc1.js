fca2741e5751916df3d64f30cada145c929f47c2
MarkusLasseigne <budderlover254@gmail.com>
2020-01-17 14:29:22 -0600

    Code tidying and gravity/player jump height tweaks

--- projects/wip-js-engine/levelc1.js
+++ projects/wip-js-engine/levelc1.js
@@ -1,95 +1,92 @@
 var player1;
-var gObstacles = [];
-var ground, skyBack;
+var gObstacles=[];
+var skyBack, ground;
 var level1 = function(){
     var y, height, width;
-    height = window.innerHeight;
-    width = window.innerWidth;
-    y = height - 100;
+    height=window.innerHeight;
+    width=window.innerWidth;
+    y=height-100;
     skyBack = new drShape(0, 0, width, height, "#aaFFFF");
     ground = new drShape(0, y, 3000, 100, "green");
     player1 = new drShape(20, y, 30, 100, "black");
-    player1.gravity = 0.05;
+    player1.gravity=1;
     refInit.start("2d", "left");
 //    console.log(player1);
 };
 
 function drShape(x, y, width, height, color, type){
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.gravity = 0;
-    this.gravitySpeed = 0;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.score = 0;
-    this.type = type;
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.gravity=0;
+    this.gravitySpeed=0;
+    this.speedX=0;
+    this.speedY=0;
+    this.score=0;
+    this.type=type;
     this.update = function(){
-        ctx = refInit.context;
-        if (this.type=="text"){
-            ctx.font = this.width + " " + this.height;
-            ctx.fillStyle = color;
+        ctx=refInit.context;
+        if(this.type=="text"){
+            ctx.font=this.width+" "+this.height;
+            ctx.fillStyle=color;
             ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
+        }else{//
+            ctx.fillStyle=color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
     }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;
+    this.newPos = function(){
+//        console.log(this.gravitySpeed, this.gravity);
+        this.gravitySpeed+=this.gravity*0.2;
+        this.x+=this.speedX;
+        this.y+=this.speedY+this.gravitySpeed;
         this.hitBottom();
     }
-    this.hitBottom = function() {
-        var rockbottom = refInit.canvas.height - this.height * 2;
+    this.hitBottom = function(){
+        var rockbottom=refInit.canvas.height-this.height*2;
 //        console.log(this.y, rockbottom);
-        if (this.y > rockbottom+0.04){
-            if(this.y == rockbottom){
-                this.speedY = this.speedY * 0.5;
-                this.gravitySpeed = 0.05;
-                this.y = rockbottom;
+        if(this.y>rockbottom+0.04){
+            if(this.y==rockbottom){
+                this.speedY=this.speedY*0.5;
+                this.gravitySpeed=0;
+                this.y=rockbottom;
 //                console.log(this.speedY, " Bounce!");
             }else{
-                this.speedY = 0;
-                this.speedX = 0;
-                this.gravitySpeed = 0.05;
-                this.y = rockbottom;
-//                console.log("grounded");
+                this.speedY=0;
+                this.speedX=0;
+                this.gravitySpeed=0;
+                this.y=rockbottom;
             }
         }
     }
 }
 
-function updateGame() {
+function updateGame(){
     var x, y, height, gap, minHeight, maxHeight, minGap, maxGap;
-//    for (i = 0; i < gObstacles.length; i += 1) {
-//        if (player1.crashWith(gObstacles[i])) {
-//            return;
-//        } 
+//    for(i=0; i<gObstacles.length; i+=1){
+//        if(player1.crashWith(gObstacles[i])){return;} 
 //    }
     refInit.clear();
-    refInit.frameNo += 1;
-    if (refInit.frameNo == 1 || everyinterval(150)) {
+    refInit.frameNo+=1;
+    if(refInit.frameNo==1 || everyinterval(150)){
         gObstacles.push();
     }
-    for (i = 0; i < gObstacles.length; i += 1) {
-        gObstacles[i].x += -1;
+    for(i=0; i<gObstacles.length; i+=1){
+        gObstacles[i].x+=-1;
         gObstacles[i].update();
     }
-    if(refInit.keys && refInit.keys[87]){player1.speedY=-3;}// w
+    if(refInit.keys && refInit.keys[87]){player1.speedY=-5;}// w
     if(refInit.keys && refInit.keys[65]){player1.speedX=-2;}// a
     if(refInit.keys && refInit.keys[83]){player1.speedY=0.1; player1.speedX=0;}// s
     if(refInit.keys && refInit.keys[68]){player1.speedX=2;}// d
-    player1.newPos();
     skyBack.update();
     ground.update();
+    player1.newPos();
     player1.update();
 //    console.log(player1);
 }
 
-function everyinterval(n) {
-    if ((refInit.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}
+function everyinterval(n){
+    if((refInit.frameNo/n) % 1==0){return true;}
+    return false;}
--- projects/wip-js-engine/refInit.js
+++ projects/wip-js-engine/refInit.js
@@ -3,63 +3,57 @@ var refInit = {
     start: function(dimention, direction, dW, dH){
         if(dimention=="2d"){
             if(direction=="down"){
-                this.canvas.width = dW || window.innerWidth;
-                this.canvas.height = dH || window.innerHeight;
-                this.context = this.canvas.getContext("2d");
+                this.canvas.width=dW || window.innerWidth;
+                this.canvas.height=dH || window.innerHeight;
+                this.context=this.canvas.getContext("2d");
                 this.frameNo=0;
                 console.log("2d down-facing time!");
-                this.interval = setInterval(updateGame, 20);
-                window.addEventListener('keydown', function (e) {
+                this.interval=setInterval(updateGame, 20);
+                window.addEventListener('keydown', function(e){
                     e.preventDefault();
-                    refInit.keys = (refInit.keys || []);
-                    refInit.keys[e.keyCode] = (e.type == "keydown");
+                    refInit.keys=(refInit.keys || []);
+                    refInit.keys[e.keyCode]=(e.type=="keydown");
                 })
-                window.addEventListener('keyup', function (e) {
-                    refInit.keys[e.keyCode] = (e.type == "keydown");
+                window.addEventListener('keyup', function(e){
+                    refInit.keys[e.keyCode]=(e.type=="keydown");
                 })
-            }
-            else if(direction=="left"){
-                this.canvas.width = dW || window.innerWidth;
-                this.canvas.height = dH || window.innerHeight;
-                this.context = this.canvas.getContext("2d");
+            }else if(direction=="left"){
+                this.canvas.width=dW || window.innerWidth;
+                this.canvas.height=dH || window.innerHeight;
+                this.context=this.canvas.getContext("2d");
                 this.frameNo=0;
                 console.log("2d left-on time!");
-                this.interval = setInterval(updateGame, 20);
-                window.addEventListener('keydown', function (e) {
+                this.interval=setInterval(updateGame, 20);
+                window.addEventListener('keydown', function(e){
                     e.preventDefault();
-                    refInit.keys = (refInit.keys || []);
-                    refInit.keys[e.keyCode] = (e.type == "keydown");
+                    refInit.keys=(refInit.keys || []);
+                    refInit.keys[e.keyCode]=(e.type=="keydown");
                 })
-                window.addEventListener('keyup', function (e) {
-                    refInit.keys[e.keyCode] = (e.type == "keydown");
+                window.addEventListener('keyup', function(e){
+                    refInit.keys[e.keyCode]=(e.type=="keydown");
                 })
-            }
-            else if(direction=="forward"){
-                this.canvas.width = dW || window.innerWidth;
-                this.canvas.height = dH || window.innerHeight;
-                this.context = this.canvas.getContext("2d");
+            }else if(direction=="forward"){
+                this.canvas.width=dW || window.innerWidth;
+                this.canvas.height=dH || window.innerHeight;
+                this.context=this.canvas.getContext("2d");
                 this.frameNo=0;
                 console.log("2d first person time!");
-                this.interval = setInterval(updateGame, 20);
-                window.addEventListener('keydown', function (e) {
+                this.interval=setInterval(updateGame, 20);
+                window.addEventListener('keydown', function(e){
                     e.preventDefault();
-                    refInit.keys = (refInit.keys || []);
-                    refInit.keys[e.keyCode] = (e.type == "keydown");
+                    refInit.keys=(refInit.keys || []);
+                    refInit.keys[e.keyCode]=(e.type=="keydown");
                 })
-                window.addEventListener('keyup', function (e) {
-                    refInit.keys[e.keyCode] = (e.type == "keydown");
+                window.addEventListener('keyup', function(e){
+                    refInit.keys[e.keyCode]=(e.type=="keydown");
                 })
             }
         }else if(dimention=="3d"){
         console.log("Woahh nelly, that'll take more time!(would be voxel based)");
         }else{console.warn("No Dimention Found");}
     },
-    stop: function() {
-        clearInterval(this.interval);
-    },  
-    clear: function(){
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+    stop: function(){clearInterval(this.interval);},  
+    clear: function(){this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);}
 }
 //used a lot from: https://www.w3schools.com/graphics/game_intro.asp and https://www.w3schools.com/tags/ref_canvas.asp
 //	if(myGameArea.keys && myGameArea.keys[37]){myGamePiece.speedX=-1;}// <
