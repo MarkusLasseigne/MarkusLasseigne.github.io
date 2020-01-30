9d74682f38127bef54f17d8b507e8f96cac79d53
MarkusLasseigne <budderlover254@gmail.com>
2020-01-29 20:44:27 -0600

    Progress on my project, slight main script optims.

--- projects/wip-js-engine/levelc1.js
+++ projects/wip-js-engine/levelc1.js
@@ -1,107 +1,81 @@
-var player1;
-var gObstacles=[];
 var skyBack, ground;
+var colObjs=[];
+var statObjs=[];
+var wallT, wall1T;
+var player1;
 var level1 = function(){
 	var y, height, width;
 	height=window.innerHeight;
 	width=window.innerWidth;
 	y=height-100;
-	skyBack = new drShape(0, 0, width, height, "#aaFFFF");
-	ground = new drShape(0, y, 3000, 100, "green");
-	player1 = new drShape(20, y, 30, 100, "black");
+	skyBack = new refInit.draw(0, 0, width, height, "#aaFFFF");
+	ground = new refInit.draw(0, y, 3000, 100, "green");
+	wallT = new refInit.draw(400, y-205, 50, 100, "black");
+	wall1T = new refInit.draw(100, y-15, 30, 30, "lime");
+	player1 = new refInit.draw(20, y+400, 30, 100, "black");
 	player1.gravity=1;
 	refInit.start("2d", "left");
 //    console.log(player1);
 };
-
-function drShape(x, y, width, height, color, type){
-	this.x=x;
-	this.y=y;
-	this.width=width;
-	this.height=height;
-	this.gravity=0;
-	this.gravitySpeed=0;
-	this.speedX=0;
-	this.speedY=0;
-	this.score=0;
-	this.type=type;
-	this.update = function(){
-		ctx=refInit.context;
-		if(this.type=="text"){
-			ctx.font=this.width+" "+this.height;
-			ctx.fillStyle=color;
-			ctx.fillText(this.text, this.x, this.y);
-		}else{//
-			ctx.fillStyle=color;
-			ctx.fillRect(this.x, this.y, this.width, this.height);
-		}
-	}
-	this.newPos = function(){
-//        console.log(this.gravitySpeed, this.gravity);
-		this.gravitySpeed+=this.gravity*0.2;
-		this.x+=this.speedX;
-		this.y+=this.speedY+this.gravitySpeed;
-		this.hitBottom();
-	}
-	this.hitBottom = function(){
-		var rockbottom=refInit.canvas.height-this.height*2;
-//        console.log(this.y, rockbottom);
-		if(this.y>rockbottom+0.04){
-			if(this.y==rockbottom){
-				this.speedY=this.speedY*0.5;
-				this.gravitySpeed=0;
-				this.y=rockbottom;
-//                console.log(this.speedY, " Bounce!");
-			}else{
-				this.speedY=0;
-				this.speedX=0;
-				this.gravitySpeed=0;
-				this.y=rockbottom;
-			}
-		}
-	}
-}
-
 function updateGame(){
 	var x, y, height, gap, minHeight, maxHeight, minGap, maxGap;
-//    for(i=0; i<gObstacles.length; i+=1){
-//        if(player1.crashWith(gObstacles[i])){return;} 
-//    }
+	player1.newPos();
 	refInit.clear();
 	refInit.frameNo+=1;
-	if(refInit.frameNo==1 || everyinterval(150)){
-		gObstacles.push();
+//	console.log(refInit.frameNo);
+	if(refInit.frameNo==1 || everyinterval(100)){
+//		statObjs.push(ground);
+		statObjs.push(wallT);
+		statObjs.push(wall1T);
 	}
-	for(i=0; i<gObstacles.length; i+=1){
-		gObstacles[i].x+=-1;
-		gObstacles[i].update();
+    for(i=0; i<statObjs.length; i+=1){
+		var myTop=player1.y; var myBottom=player1.y+(player1.height);
+		var myLeft=player1.x; var myRight=player1.x+(player1.width);
+		var myWidthH=player1.width/1.2; var myHeightH=player1.height/1.2;
+		var otTop=statObjs[i].y; var otBottom=statObjs[i].y+statObjs[i].height;
+		var otLeft=statObjs[i].x; var otRight=statObjs[i].x+statObjs[i].width;
+		if(myTop<otBottom && !(myTop<otTop+myHeightH) && !(myBottom<otTop) && myRight>otLeft+2 && myLeft<otRight-2){//my top collide
+			player1.pHitTop(otBottom);}
+		if(myBottom>=otTop && !(myBottom>otBottom) && (myTop<otBottom) && myRight>otLeft+2 && myLeft<otRight-2){//my bottom collide
+			player1.pHitBottom(otTop);}
+		if(myLeft<=otRight && (myLeft>otLeft+myWidthH) && !(myRight<otLeft) && myTop<otBottom+2 && myBottom>otTop-2){//my left collide
+			player1.pHitLeft(otRight);}
+		if(myRight>=otLeft && (myRight<otRight-myWidthH) && (myLeft<otRight) && myTop<otBottom-2 && myBottom>otTop+2){//my right collide
+			player1.pHitRight(otLeft);}
 	}
-	if(refInit.keys && refInit.keys[87]){player1.speedY=-5;}// w
-	if(refInit.keys && refInit.keys[65]){player1.speedX=-2;}// a
-	if(refInit.keys && refInit.keys[83]){player1.speedY=0.1; player1.speedX=0;}// s
-	if(refInit.keys && refInit.keys[68]){player1.speedX=2;}// d
-
-	if(refInit.keys && refInit.keys[38]){player1.speedY=-5;}// ^
-	if(refInit.keys && refInit.keys[37]){player1.speedX=-2;}// <
-	if(refInit.keys && refInit.keys[40]){player1.speedY=0.1; player1.speedX=0;}// v
-	if(refInit.keys && refInit.keys[39]){player1.speedX=2;}// >
-
+	if(refInit.keys && refInit.keys["ArrowLeft"]){player1.speedX=-2;}// <
+	if(refInit.keys && refInit.keys["a"]){player1.speedX=-2;}// a
+	if(refInit.keys && refInit.keys["ArrowRight"]){player1.speedX=2;}// >
+	if(refInit.keys && refInit.keys["d"]){player1.speedX=2;}// d
+	if(refInit.keys && refInit.keys["ArrowUp"]){player1.speedY=-50;}// ^
+	if(refInit.keys && refInit.keys["w"]){player1.speedY=-5;}// w
+	if(refInit.keys && refInit.keys["ArrowDown"]){player1.speedY=0.1; player1.speedX=0;}// v
+	if(refInit.keys && refInit.keys["s"]){player1.speedY=0.1; player1.speedX=0;}// s
+	for(i=0; i<statObjs.length; i+=1){
+//		statObjs[i].x+=-1;
+		statObjs[i].update();
+	}
 	skyBack.update();
-	ground.update();
-	player1.newPos();
+	wallT.update();
+	wall1T.update();
 	player1.update();
-//    console.log(player1);
+	ground.update();
 }
 
 function everyinterval(n){
 	if((refInit.frameNo/n) % 1==0){return true;}
 	return false;}
 
-//	if(refInit.keys && refInit.keys[37]){player1.speedX=-1;}// <
-//	if(refInit.keys && refInit.keys[65]){player1.speedX=-1;}// a
-//	if(refInit.keys && refInit.keys[39]){player1.speedX=1;}// >
-//	if(refInit.keys && refInit.keys[68]){player1.speedX=1;}// d
-//	if(refInit.keys && refInit.keys[38]){player1.speedY=-1;}// ^
-//	if(refInit.keys && refInit.keys[87]){player1.speedY=-1;}// w
-//	if(refInit.keys && refInit.keys[40]){player1.speedY=1;}// v
-//	if(refInit.keys && refInit.keys[83]){player1.speedY=1;}// s
+//	if(refInit.keys && refInit.keys[37]){player1.speedX=-2;}// <
+//	if(refInit.keys && refInit.keys[65]){player1.speedX=-2;}// a
+//	if(refInit.keys && refInit.keys[39]){player1.speedX=2;}// >
+//	if(refInit.keys && refInit.keys[68]){player1.speedX=2;}// d
+//	if(refInit.keys && refInit.keys[38]){player1.speedY=-5;}// ^
+//	if(refInit.keys && refInit.keys[87]){player1.speedY=-5;}// w
+//	if(refInit.keys && refInit.keys[40]){player1.speedY=0.1; player1.speedX=0;}// v
+//	if(refInit.keys && refInit.keys[83]){player1.speedY=0.1; player1.speedX=0;}// s
+
+//if(refInit.key && refInit.key=="w"||refInit.key=="ArrowUp"){player1.speedY=-5;}// w
+//if(refInit.key && refInit.key=="a"||refInit.key=="ArrowLeft"){player1.speedX=-2;}// a
+//if(refInit.key && refInit.key=="s"||refInit.key=="ArrowDown"){player1.speedY=0.1; player1.speedX=0;}// s
+//if(refInit.key && refInit.key=="d"||refInit.key=="ArrowRight"){player1.speedX=2;}// d
--- projects/wip-js-engine/refInit.js
+++ projects/wip-js-engine/refInit.js
@@ -1,6 +1,16 @@
 var refInit = {
 	canvas: document.querySelector("#daCan"),
 	start: function(dimention, direction, dW, dH){
+		window.addEventListener('keydown', function(e){
+			e.preventDefault();
+			refInit.keys = (refInit.keys || []);
+			refInit.keys[e.key] = (e.type == "keydown");
+			optT(e);
+		})
+		window.addEventListener('keyup', function(e){
+			refInit.keys[e.key] = (e.type == "keydown");
+			optT(e);
+		})
 		if(dimention=="2d"){
 			if(direction=="down"){
 				this.canvas.width=dW || window.innerWidth;
@@ -9,14 +19,6 @@ var refInit = {
 				this.frameNo=0;
 				console.log("2d down-facing time!");
 				this.interval=setInterval(updateGame, 20);
-				window.addEventListener('keydown', function(e){
-					e.preventDefault();
-					refInit.keys=(refInit.keys || []);
-					refInit.keys[e.keyCode]=(e.type=="keydown");
-				})
-				window.addEventListener('keyup', function(e){
-					refInit.keys[e.keyCode]=(e.type=="keydown");
-				})
 			}else if(direction=="left"){
 				this.canvas.width=dW || window.innerWidth;
 				this.canvas.height=dH || window.innerHeight;
@@ -24,14 +26,6 @@ var refInit = {
 				this.frameNo=0;
 				console.log("2d left-on time!");
 				this.interval=setInterval(updateGame, 20);
-				window.addEventListener('keydown', function(e){
-					e.preventDefault();
-					refInit.keys=(refInit.keys || []);
-					refInit.keys[e.keyCode]=(e.type=="keydown");
-				})
-				window.addEventListener('keyup', function(e){
-					refInit.keys[e.keyCode]=(e.type=="keydown");
-				})
 			}else if(direction=="forward"){
 				this.canvas.width=dW || window.innerWidth;
 				this.canvas.height=dH || window.innerHeight;
@@ -39,20 +33,74 @@ var refInit = {
 				this.frameNo=0;
 				console.log("2d first person time!");
 				this.interval=setInterval(updateGame, 20);
-				window.addEventListener('keydown', function(e){
-					e.preventDefault();
-					refInit.keys=(refInit.keys || []);
-					refInit.keys[e.keyCode]=(e.type=="keydown");
-				})
-				window.addEventListener('keyup', function(e){
-					refInit.keys[e.keyCode]=(e.type=="keydown");
-				})
 			}
 		}else if(dimention=="3d"){
 		console.log("Woahh nelly, that'll take more time!(would be voxel based)");
 		}else{console.warn("No Dimention Found");}
 	},
 	stop: function(){clearInterval(this.interval);},  
-	clear: function(){this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);}
+	clear: function(){this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);},
+	draw: function(x, y, width, height, color, type){
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		this.gravity=0;
+		this.gravitySpeed=0;
+		this.speedX=0;
+		this.speedY=0;
+		this.score=0;
+		this.type=type;
+		this.update = function(){
+			ctx=refInit.context;
+			if(this.type=="text"){
+				ctx.font=this.width+" "+this.height;
+				ctx.fillStyle=color;
+				ctx.fillText(this.text, this.x, this.y);
+			}else{//
+				ctx.fillStyle=color;
+				ctx.fillRect(this.x, this.y, this.width, this.height);
+			}
+		}
+		this.newPos = function(){
+			this.gravitySpeed+=this.gravity*0.2;
+			this.x+=this.speedX;
+			this.y+=this.speedY+this.gravitySpeed;
+			this.hitBottom();
+		}
+		this.pHitBottom = function(whaBot){
+			this.speedX=0;
+			this.speedY=0;
+			this.y=whaBot-player1.height;
+			this.gravitySpeed=0;
+		}
+		this.pHitTop = function(whaBot){
+			this.speedY=0;
+			this.y=whaBot;
+		}
+		this.pHitLeft = function(whaBot){
+			this.speedX=0;
+			this.x=whaBot;
+		}
+		this.pHitRight = function(whaBot){
+			this.speedX=0;
+			this.x=whaBot-this.width;
+		}
+		this.hitBottom = function(){
+			var rockbottom=refInit.canvas.height-this.height*2;
+			if(this.y>rockbottom+0.04){
+				if(this.y==rockbottom){
+					this.speedY=this.speedY*0.5;
+					this.gravitySpeed=0;
+					this.y=rockbottom;
+				}else{
+					this.speedY=0;
+					this.speedX=0;
+					this.gravitySpeed=0;
+					this.y=rockbottom;
+				}
+			}
+		}
+	}
 }
 //used a lot from: https://www.w3schools.com/graphics/game_intro.asp and https://www.w3schools.com/tags/ref_canvas.asp
