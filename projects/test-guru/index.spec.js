ebacce1cdc79dcd7f06b33ff311895b6eac6efa1
Ubuntu <ubuntu@ip-172-31-86-243.ec2.internal>
2020-02-22 00:12:59 +0000

    pre submit transfer from C9 to VSC

--- projects/test-guru/index.spec.js
+++ projects/test-guru/index.spec.js
@@ -7,54 +7,46 @@ if ((typeof process !== 'undefined') &&
 	expect = require('chai').expect;
 }
 
-describe("Test Guru", function () {
-	
+describe("Test Guru", function(){
 	/* global expect, sinon*/
 	
-	beforeEach(function () {
+	beforeEach(function(){
 		sinon.spy(console, 'log');
 	});
 
-	afterEach(function () {
+	afterEach(function(){
 		console.log.restore();
 	});
 
-	it('Our First Test', function () {
-		var value = "hello tests";
-		var some_number = 484;
+	it('Our First Test', function(){
+		var value="hello tests";
+		var some_number=484;
 
-		//                 ┌ Change this to what it should be
-		expect(value === '???').to.be.true;
-		
-		//                        ┌ Change this to what it should be
-		expect(some_number === '???').to.be.true;
+		expect(value === 'hello tests').to.be.true;
+		expect(some_number === 484).to.be.true;
 	});
 
 	it("Functions can access/modify variables in parent scope.", function(){
-		var outside_the_function = null;
+		var outside_the_function=null;
 		function yay(){
-			var inside_the_function = "can you see me?";
-			outside_the_function = inside_the_function;
+			var inside_the_function="can you see me?";
+			outside_the_function=inside_the_function;
 		}
-
-		yay();
-		
-		expect(outside_the_function === '???').to.be.true;
+		yay(); expect(outside_the_function === 'can you see me?').to.be.true;
 	});
 
 	it("Function Parameters become scoped to the function.", function(){
-
 		function yay(param){
-			expect(param === '???').to.be.true;
+			expect(param === 'a fine kettle of fish').to.be.true;
 		}
-
 		yay("a fine kettle of fish");
 	});
 
 	it("A functions local scope is not available in an outer scope.", function(){
+		
 		function yay(){
-			var kix = "kid tested mother approved";
-			expect(kix === '???').to.be.true;
+			var kix="kid tested mother approved";
+			expect(kix === 'kid tested mother approved').to.be.true;
 		}
 		yay();
 		
@@ -63,124 +55,87 @@ describe("Test Guru", function () {
 		// "this" is a special object that by default represents the global scope.
 		// variables declared globally are stored as a property on the object: this.<variable>
 		// if the variable is not global then this.<variable> will be undefined
-		if(this.kix !== undefined){
-			has_kix = kix;
-		} else {
-			has_kix = "i prefer cheerios";
-		}
-		expect(has_kix === '???').to.be.true;
+		if(this.kix !== undefined){has_kix=kix;}
+		else{has_kix="i prefer cheerios";}
+		expect(has_kix === 'i prefer cheerios').to.be.true;
 	});
 
 	it("Functions don't have access to eachothers scope", function(){
-		function yay(){
-			var from_yay = "i'm inside yay;";
-		}
+		function yay(){this.from_yay="i'm inside yay;";}
 
 		function foo(){
-			var in_foo = "i'm in foo";
-			if(this.from_yay !== undefined){
-				in_foo = this.from_yay;
-			}
-			expect(in_foo === '???').to.be.true;
-			expect(this.from_yay === '???').to.be.true;
+			var in_foo="i'm in foo";
+			if(this.from_yay !== undefined){in_foo=this.from_yay;}
+			expect(in_foo === "i'm inside yay;").to.be.true;
+			expect(this.from_yay === "i'm inside yay;").to.be.true;
 		}
 		yay();
 		foo();
 	});
 
 	it("Inner scope variables have preference over outter scope variables with the same name.", function(){
-
-		var peanuts = 300;
+		var peanuts=300;
 
 		function yay(){
-			var peanuts = "roasted";
-
-			expect(peanuts === '???').to.be.true;
+			var peanuts="roasted";
+			expect(peanuts === 'roasted').to.be.true;
 		}
-		yay();
-
-		expect(peanuts === '???').to.be.true;
+		yay(); expect(peanuts === 300).to.be.true;
 	});
 
 	it("Variables created with var in a funtion are re-created each time", function(){
 		function yay(){
-			if(this.counter !== undefined){
-				counter = counter + 1;
-			} else {
-				var counter = 10;
-			}
+			if(this.counter !== undefined){counter=counter + 1;}
+			else{var counter=10;}
 		}
-
-		yay();
-		expect(this.counter === '???').to.be.true;
-		yay();
-		expect(this.counter === '???').to.be.true;
-		yay();
-		expect(this.counter === '???').to.be.true;
+		yay(); expect(this.counter === undefined).to.be.true;
+		yay(); expect(this.counter === undefined).to.be.true;
+		yay(); expect(this.counter === undefined).to.be.true;
 	});
 
 	it("Inner scope can access outer scope", function(){
-		var im_outside = "alpha";
+		var im_outside="alpha";
 		function yay(){
-			var im_inside = "omega";
+			var im_inside="omega";
 			return im_outside + im_inside;
 		}
-
-		expect(yay() === '???').to.be.true;
+		expect(yay() === 'alphaomega').to.be.true;
 	});
 
 	it("Functions retain outer scope references between calls.", function(){
-		var im_outside = 13;
+		var im_outside=13;
 		function yay(){
-			im_outside += 1;
+			im_outside +=1;
 		}
 
-		yay();
-		expect(im_outside === '???').to.be.true;
-		yay();
-		expect(im_outside === '???').to.be.true;
+		yay(); expect(im_outside === 14).to.be.true;
+		yay(); expect(im_outside === 15).to.be.true;
 	});
 
 	it("We can do goofy stuff with outer scope", function(){
-
-		var hello = "greg";
-		var name = "";
-
-		function yay(){
-			name += hello;
-		}
-
-		yay();
-		expect(name === '???').to.be.true;
-		yay();
-		expect(name === '???').to.be.true;
-		yay();
-		expect(name === '???').to.be.true;
-
+		var hello="greg";
+		var name="";
+		
+		function yay(){name +=hello;}
+		yay(); expect(name === 'greg').to.be.true;
+		yay(); expect(name === 'greggreg').to.be.true;
+		yay(); expect(name === 'greggreggreg').to.be.true;
 	});
 
 	it("We can pass functions to other functions and then run them.", function(){
-		var im_outter = 10;
-		function yay(){
-			im_outter /= 5;
-		}
+		var im_outter=10;
+		function yay(){im_outter /=5;}
 		function something(whatever){
-			im_outter *= 20;
+			im_outter *=20;
 			whatever();
 		}
-		something(yay);
-		
-		expect(im_outter === '???').to.be.true;
+		something(yay);	expect(im_outter === 40).to.be.true;
 	});
 
 	it("We can get crazy with returns.", function(){
-		function yay(){
-			return " is dog";
-		}
-		function foo(whatever){
-			return "hello, this" + whatever();
-		}
+		function yay(){return " is dog";}
 
-		expect(foo(yay) === '???').to.be.true;
+		function foo(whatever){return "hello, this" + whatever();}
+		expect(foo(yay) === 'hello, this is dog').to.be.true;
 	});
 });
