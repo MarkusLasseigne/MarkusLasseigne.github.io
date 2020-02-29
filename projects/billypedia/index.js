2c1f3289157a3070b365c06178244436bd055d18
MarkusLasseigne <budderlover254@gmail.com>
2020-02-28 22:04:30 -0600

    Billypedia progress near done.

--- projects/billypedia/index.js
+++ projects/billypedia/index.js
@@ -1,19 +1,55 @@
 /* global $ _ opspark */
 
-$(document).ready(function() {
-    $.getJSON('data.json', function (data) {
-        // YOUR CODE BELOW HERE //
-
-        // uncomment this to inspect all available data; delete when done //
-
-        // EXAMPLE: Looping over top rated recordings; replace with your code //
-        // let topRated = data.discography.topRated;
-        // _.forEach(topRated, function(recording) {
-        //     console.log(recording);
-        // });
-
-
-        // YOUR CODE ABOVE HERE //
-    })
-    .fail(function() { console.log('getJSON on discography failed!'); });
+$(document).ready(function(){
+	$.getJSON('data.json', function (data){
+		let topData = data.discography.topRated;
+		let recData = data.discography.recordings;
+		let billyData = data.images.billy;
+		let riderData = data.rider;
+		var billyI=0;
+		$('#list-top-rated').append($('<div id="image-container-top-rated" class="image-container">')
+			.append($('<img id="top-rated-image" class="image">').attr("src", topData[0].art)));
+		topData.map(x =>$('#list-top-rated')
+			.append($('<li>')
+				.attr("class", "top-rated")
+				.attr("click-val", x.title.toLowerCase().replace(/ /g,"-"))
+				.text(x.title)
+			)
+		);
+		$('#sidebar').append($('<section id="section-recordings">')
+			.append($('<div id="image-container-recording" class="image-container">')
+				.append($('<img id="recording-image" class="image">')
+					.attr("src", recData[0].art)
+				)
+			)
+			.append($('<ul id="list-recordings" class="list-recordings">'))
+		);
+		recData.map(x =>$('#list-recordings')
+			.append($('<li class="recording">')
+				.attr("click-val", x.title.toLowerCase().replace(/ /g, "-")
+				).append($('<div class="title">').text("Title: "+x.title)
+				).append($('<div class="artist">').text("Artist: "+x.artist)
+				).append($('<div class="release">').text("Release: "+x.release)
+				).append($('<div class="year">').text("Year: "+x.year))
+			)
+		);
+		$('#image-container-billy').on("click", function(){
+			if(billyI>=3){billyI=0;	$('#image-billy').attr("src", billyData[billyI]);}
+			else{billyI++; $('#image-billy').attr("src", billyData[billyI]);}
+		});
+		$('.top-rated').on("click", function(){
+			var imageURL = "images/album/"+$(this).attr('click-val')+".jpg";
+			$('#top-rated-image').attr("src", imageURL);
+		});
+		$('.recording').on("click", function(){
+			var imageURL = "images/album/"+$(this).attr('click-val')+".jpg";
+			$('#recording-image').attr("src", imageURL);
+		});
+		$('#bId').append($('<div class="rider">').append($('<table>').append($('<tbody>'))));
+		riderData.map(x =>$('tbody')
+			.append($('<tr>').append($('<td>').text(x.type)).append($('<td>').text(x.desc))
+			)
+		);
+	}) // YOUR CODE ABOVE HERE //
+	.fail(function(){console.log('getJSON on discography failed!');});
 });
--- projects/billypedia/test/index.spec.js
+++ projects/billypedia/test/index.spec.js
@@ -17,35 +17,35 @@ app.use(express.static(path.join(__dirname, '../')));
 
 describe('Billypedia', function(){
 
-  after(done => server.close(done));
+	after(done => server.close(done));
 
-  beforeEach(function(done) {
-    browser.visit(url, done);
-  });
+	beforeEach(function(done) {
+		browser.visit(url, done);
+	});
 
-  it('should have a title Billypedia', function(done){
-    browser.assert.text('title', 'Billypedia');
-    done();
-  });
+	it('should have a title Billypedia', function(done){
+		browser.assert.text('title', 'Billypedia');
+		done();
+	});
 
-  it('should have one image of billy', function(done){
-    browser.assert.elements('#image-billy', 1);
-    done();
-  });
+	it('should have one image of billy', function(done){
+		browser.assert.elements('#image-billy', 1);
+		done();
+	});
 
-  it('should have three images on page', function(done){
-    browser.assert.elements('img', {atLeast: 3}, "Make sure you have an <img> for Billy, top-rated albums, and recording albums");
-    done();
-  });
+	it('should have three images on page', function(done){
+		browser.assert.elements('img', {atLeast: 3}, "Make sure you have an <img> for Billy, top-rated albums, and recording albums");
+		done();
+	});
 
-  it('should change image of Billy when clicked', function(done){
-    data.images.billy.forEach(function(src) {
-      browser.assert.attribute('#image-billy', 'src', src);
-      browser.click('#image-billy');
-    })
-    browser.assert.attribute('#image-billy', 'src', data.images.billy[0]);
-    done();
-  });
+	it('should change image of Billy when clicked', function(done){
+		data.images.billy.forEach(function(src) {
+			browser.assert.attribute('#image-billy', 'src', src);
+			browser.click('#image-billy');
+		})
+		browser.assert.attribute('#image-billy', 'src', data.images.billy[0]);
+		done();
+	});
 
 	it('should have five top rated titles', function(done){
 		browser.assert.elements('#list-top-rated > *', 5, 'Make sure you append a child to "#list-top-rated" for each of the top-rated albums');
@@ -54,17 +54,17 @@ describe('Billypedia', function(){
 		done();
 	});
 
-  it('should change top rated album image when clicking an album name', function(done){
-    const src = num => data.discography.topRated[num].art;
-    browser.assert.elements('#section-top-rated > div', 1, 'Make sure your "section-top-rated" list has a <div>');
-    browser.assert.elements('#section-top-rated img', 1, 'Make sure your <img> is in the "#section-top-rated" section');
-    browser.assert.attribute('#section-top-rated img', 'src', src(0), 'Make sure the src for your <img> defaults to the first image in the list');
-    data.discography.topRated.forEach(function(album, index) {
-      browser.click(`#section-top-rated > ul > *:nth-of-type(${index + 1})`);
-      browser.assert.attribute('#section-top-rated > div#image-container-top-rated > img', 'src', album.art, 'Make sure your <img>\'s src updates dymanically');
-    })
-    done();
-  });
+	it('should change top rated album image when clicking an album name', function(done){
+		const src = num => data.discography.topRated[num].art;
+		browser.assert.elements('#section-top-rated > div', 1, 'Make sure your "section-top-rated" list has a <div>');
+		browser.assert.elements('#section-top-rated img', 1, 'Make sure your <img> is in the "#section-top-rated" section');
+		browser.assert.attribute('#section-top-rated img', 'src', src(0), 'Make sure the src for your <img> defaults to the first image in the list');
+		data.discography.topRated.forEach(function(album, index) {
+			browser.click(`#section-top-rated > ul > *:nth-of-type(${index + 1})`);
+			browser.assert.attribute('#section-top-rated > div#image-container-top-rated > img', 'src', album.art, 'Make sure your <img>\'s src updates dymanically');
+		})
+		done();
+	});
 
 	it('should have a section with the id "section-recordings"', function(done){
 		browser.assert.elements('section#section-recordings', 1, 'Make sure you have a <section> with the ID "section-recordings"');
@@ -83,22 +83,22 @@ describe('Billypedia', function(){
 		done();
 	});
 
-  it('should change recordings album image when clicking an album name', function(done){
-    const src = num => data.discography.recordings[num].art;
-    browser.assert.elements('#section-recordings > div', 1, 'Make sure your "section-recordings" list has a <div>');
-    browser.assert.elements('#section-recordings img', 1, 'Make sure your <img> is inside the "#section-recordings" section');
-    browser.assert.attribute('#section-recordings img', 'src', src(0), 'Make sure the src for your <img> defaults to the first image in the list');
-    data.discography.recordings.forEach(function(album, index) {
-      browser.click(`#section-recordings > ul > *:nth-of-type(${index + 1})`);
-      browser.assert.attribute('#section-recordings > div#image-container-recording > img', 'src', album.art, 'Make sure your image\'s src updates dymanically');
-    })
-    done();
-  });
+	it('should change recordings album image when clicking an album name', function(done){
+		const src = num => data.discography.recordings[num].art;
+		browser.assert.elements('#section-recordings > div', 1, 'Make sure your "section-recordings" list has a <div>');
+		browser.assert.elements('#section-recordings img', 1, 'Make sure your <img> is inside the "#section-recordings" section');
+		browser.assert.attribute('#section-recordings img', 'src', src(0), 'Make sure the src for your <img> defaults to the first image in the list');
+		data.discography.recordings.forEach(function(album, index) {
+			browser.click(`#section-recordings > ul > *:nth-of-type(${index + 1})`);
+			browser.assert.attribute('#section-recordings > div#image-container-recording > img', 'src', album.art, 'Make sure your image\'s src updates dymanically');
+		})
+		done();
+	});
 
-  it('should have a table for all rider data', function(done){
-    browser.assert.elements('table', 1, "Make sure you have a <table>");
-    browser.assert.elements('tr', 14);
-    browser.assert.elements('td', 28);
-    done();
-  });
+	it('should have a table for all rider data', function(done){
+		browser.assert.elements('table', 1, "Make sure you have a <table>");
+		browser.assert.elements('tr', 14);
+		browser.assert.elements('td', 28);
+		done();
+	});
 });
